35c444247880b6032b715ba9c88548ca35340cff
dlion <domenico.luciani@email.it>
2012-08-31 19:10:32 +0200

    Library for usefull function that i wrote.

--- lib/funzioni.c
+++ lib/funzioni.c
@@ -6,7 +6,7 @@
 #  
 # 
 # This file may be licensed under the terms of of the
-# GNU General Public License Version 2 (the ``GPL'').
+# GNU General Public License Version 3 (the ``GPL'').
 # 
 # Software distributed under the License is distributed
 # on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY KIND, either
